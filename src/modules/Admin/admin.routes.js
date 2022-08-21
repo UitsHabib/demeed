8fed7293c5a0b001cbcef5b5524072ab461ab8b7
tanvinpavel <tanvinpavel123@gmail.com>
2022-08-21 12:07:01 +0600

    Admin Signin & Signup Features

--- src/modules/Admin/admin.routes.js
+++ src/modules/Admin/admin.routes.js
@@ -2,7 +2,7 @@ const { signup, signing } = require('./admin.controllers');
 
 const adminRoute = (app) => {
     app.post('/admin/register', signup);
-
+    feature
     app.post('/admin/login', signing);
 };
 
