a7d4e95e909252ee03ea6d97784c9cfc8281b953
bappytuhin <bappytuhin0204@gmail.com>
2022-09-04 18:05:14 +0600

    Create Token

--- src/config/lib/express.js
+++ src/config/lib/express.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
 const config = require('../index.js');
+const cookieparser = require("cookie-parser");
 
 
 module.exports = function (){
     const app= express();
 
     app.use(express.json());
+    app.use(cookieparser("cookie-secret"));
 
 
     const globalConfig = config.getGlobalConfig();
--- src/modules/admin/admin.controller.js
+++ src/modules/admin/admin.controller.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const Admin = require('./admin.model.js');
 const admin = [
     {
@@ -31,5 +32,54 @@ function createAdmin(req,res){
     
 };
 
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = {
+        email,
+        password
+    };
+
+    const promise = Admin.findOne({
+        where: {
+            email: user.email,
+            password: user.password
+        },
+    });
+
+    function success(user) {
+        console.log(user);
+        if (user) {
+            const access_token = jwt.sign(
+                {
+                    id: user.id
+                },
+                "mysecret",
+                {
+                    expiresIn: "1h",
+                    issuer: user.id.toString()
+                }
+            );
+
+            res.cookie("access-token", access_token, {
+                httpOnly: true,
+                signed: true
+            });
+
+            res.status(200).json(user);
+        } else {
+            res.status(404).send("User not found.");
+        }
+    };
+
+    function err(err) {
+        console.log(err);
+        res.status(500).send("Internal server error.");
+    }
+
+    promise.then(success).catch(err);
+}
+
 module.exports.getAdmins = getAdmins;
 module.exports.createAdmin = createAdmin;
+module.exports.login = login;
