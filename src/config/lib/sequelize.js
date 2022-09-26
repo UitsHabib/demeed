dce6630b951e557c10142af7c8bd182ad1c67242
sbswarna <Shatabdi.biswas@brainstation-23.com>
2022-09-26 17:27:12 +0600

    Add Env File

--- src/config/lib/sequelize.js
+++ src/config/lib/sequelize.js
@@ -1,10 +1,12 @@
-const { Sequelize } = require('sequelize');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const { DB_HOST, DB_USER, DB_PASSWORD } = process.env;
 
-const sequelize = new Sequelize('blog', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false,
-    sync: true,
-})
+const sequelize = new Sequelize("blog", DB_USER, DB_PASSWORD, {
+	host: DB_HOST,
+	dialect: "mysql",
+	logging: false,
+	sync: true,
+});
 
 module.exports = sequelize;
