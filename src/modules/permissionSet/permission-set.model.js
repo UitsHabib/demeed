d0cb5a9282439efe06d86f98eaa2d8a2121316d2
amanullhazoha <amaullhazoha3784@gmail.com>
2022-09-27 22:45:11 +0600

    Resolve Conflict

---
+++ src/modules/permissionSet/permission-set.model.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const sequelize = require(path.join(process.cwd(), "src/config/lib/sequelize.js"));
+const { DataTypes } = require("sequelize");
+
+const PermissionSet = sequelize.define(
+    "permission-sets",
+    {
+        id: {
+            allowNull: false,
+            primaryKey: true,
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4
+        },
+        name: {
+            type: DataTypes.STRING
+        },
+        description: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        tableName: "permission-sets",
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+);
+
+module.exports = PermissionSet;
