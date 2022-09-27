cf9d3bcb3737a43365c7f153c7d9c8b4949b48d7
amanullhazoha <amaullhazoha3784@gmail.com>
2022-09-27 22:56:10 +0600

    Resolve Conflict

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -1,11 +1,11 @@
-const validator = require("../core/middleware/validator.middleware");
+const validate = require("../core/middlewares/validate.middleware");
 const { signUp,login, getSignedInUserProfile, logout } = require("./admin.controller");
 const { signUpSchema, loginSchema } = require("./admin.schema");
 const AdminStrategy = require("./admin.authentication.middelware");
 
 module.exports = (app) => {
-    app.post("/api/admins/signup", validator(signUpSchema), signUp);
-    app.post("/api/admins/login", validator(loginSchema), login);
+    app.post("/api/admins/signup", validate(signUpSchema), signUp);
+    app.post("/api/admins/login", validate(loginSchema), login);
     app.get("/api/admins/profile", AdminStrategy, getSignedInUserProfile);
     app.get("/api/admins/logout", logout);
 };
