29c4215ac49b4f80fd71c0d9b85e70b70b382e30
amanullhazoha <amaullhazoha3784@gmail.com>
2022-09-27 15:13:17 +0600

    Resolve Conflict

--- src/modules/permission/permission.routes.js
+++ src/modules/permission/permission.routes.js
@@ -1,7 +1,14 @@
-const { createPermission } = require("./permission.controller");
-
-const UserStrategy = require("../user/user.authentication.middleware");
+const validator = require("../core/middleware/validator.middleware");
+const { getPermission, createPermission,updatePermission, deletePermission } = require("./permission.controller");
+const AdminStrategy = require("../admin/admin.authentication.middelware");
+const { permissionSchema, permissionUpdateSchema } = require("./permission.schema");
 
 module.exports = (app) => {
-  app.post("/api/permissions", UserStrategy, createPermission);
+    app.route("/api/permissions")
+        .get(AdminStrategy, getPermission)
+        .post(AdminStrategy, validator(permissionSchema), createPermission);
+
+    app.route("/api/permissions/:id")
+        .patch(AdminStrategy, validator(permissionUpdateSchema), updatePermission)
+        .delete(AdminStrategy, deletePermission);
 };
